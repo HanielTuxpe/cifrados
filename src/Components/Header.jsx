4af1c11357263ce88f9bfeedf86150eea40ddad1
HanielTuxpe <hatg0504@gmail.com>
2024-10-03 11:17:00 -0600

    XD

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { jsPDF } from "jspdf";
+import { toast } from 'react-toastify';
 
 const Header = ({ darkMode, toggleDarkMode }) => {
+
+  const generarPDF = () => {
+    const doc = new jsPDF();
+
+    doc.setFont('Typo Formal Bold Italic Demo');
+
+    // Agregar el título
+    doc.setFontSize(20);
+    doc.text(`Re`, doc.internal.pageSize.getWidth() / 2, 15, { align: 'center' });
+
+    doc.save(`reporte_.pdf`);
+    toast.success('Generando reporte PDF de los últimos estados...');
+  };
+
   return (
     <header id="top" className="App-header">
-      <div >
+      <div>
         <div className="header-title">
           <h1>Métodos de Cifrado</h1>
           <h4>Simétricos, Asimétricos, Hash, de desplazamiento y de transposición </h4>
@@ -28,6 +44,7 @@ const Header = ({ darkMode, toggleDarkMode }) => {
           <li><a href="#AES-section">Cifrado AES</a></li>
           <li><a href="#RSA-section">Cifrado RSA</a></li>
           <li><a href="#MD5-section">MD5</a></li>
+          <li id="Reporte"><a href="#Reporte" onClick={generarPDF}>Reporte</a></li>
         </ul>
       </nav>
     </header>
